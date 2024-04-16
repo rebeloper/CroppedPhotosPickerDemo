55d5d70eaeae797af0b40e3e4c17fdf900dd30aa
Alex Nagy <rebeloperalpar@gmail.com>
2024-04-16 19:21:02 +0300

    Initial Commit

---
+++ CroppedPhotosPickerDemo/CroppedPhotosPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CroppedPhotosPickerDemoApp.swift
+//  CroppedPhotosPickerDemo
+//
+//  Created by Alex Nagy on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CroppedPhotosPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
